f8854f0094a0da0c97b6cb94fedf6a0a39287dfd
froncast <froncast@alt-point.ru>
2022-10-06 12:30:43 +0500

    move TypeOrm to a separate module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,9 @@
 import { Module, CacheModule } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
-import { TypeOrmModule } from '@nestjs/typeorm'
 import * as redisStore from 'cache-manager-redis-store'
 import { AuthModule } from './modules/auth/auth.module'
 import { EmailModule } from './infrastructure/emailer/emailer.module'
-import { Communication } from './modules/communications/domain/entities/communications.entity'
-import { Comment } from './modules/comments/domain/entities/comments.entity'
-import { Event } from './modules/events/domain/entities/events.entity'
-import { Resource } from './modules/resources/domain/entities/resources.entity'
-import { Tag } from './modules/tags/domain/entities/tags.entity'
-import { User } from './modules/users/domain/entities/users.entity'
-import { Venue } from './modules/venues/domain/entities/venues.entity'
-import { CommunicationConfirm } from './modules/verification-codes/domain/entities/communication-confirm.entity'
-import { VenueScheduleItem } from './modules/venues/domain/entities/venueScheduleItem.entity'
 import { MapperModule } from './infrastructure/automapper/mapper.module'
 import { ResourcesModule } from './modules/resources/resources.module'
 import { TagsModule } from './modules/tags/tags.module'
@@ -21,41 +11,17 @@ import { VenuesModule } from './modules/venues/venues.module'
 import { EventsModule } from './modules/events/events.module'
 import { CommunicationsModule } from './modules/communications/communications.module'
 import { CommentsModule } from './modules/comments/comments.module'
+import { TOrmModule } from './infrastructure/database/typeOrm.module'
 
 @Module({
 	imports: [
-		TypeOrmModule.forFeature([
-			Comment,
-			Communication,
-			Event,
-			Resource,
-			Tag,
-			User,
-			Venue,
-			VenueScheduleItem,
-			CommunicationConfirm,
-		]),
 		ConfigModule.forRoot({ isGlobal: true }),
 		CacheModule.register({
 			isGlobal: true,
 			store: redisStore,
 			host: process.env.REDIS_HOST,
 		}),
-		TypeOrmModule.forRoot({
-			type: 'postgres',
-
-			host: process.env.DB_HOST,
-			port: Number(process.env.DB_PORT),
-
-			username: process.env.DB_USER,
-			password: process.env.DB_PASS,
-			database: process.env.DB_NAME,
-
-			synchronize: process.env.DB_SYNC !== 'false',
-			logging: process.env.DB_DEBUG !== 'false',
-
-			autoLoadEntities: true,
-		}),
+		TOrmModule,
 		MapperModule,
 		EmailModule,
 		AuthModule,
---
+++ src/infrastructure/database/typeOrm.module.ts
@@ -0,0 +1,47 @@
+import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Comment } from "src/modules/comments/domain/entities/comments.entity";
+import { Communication } from "src/modules/communications/domain/entities/communications.entity";
+import { Event } from "src/modules/events/domain/entities/events.entity";
+import { Resource } from "src/modules/resources/domain/entities/resources.entity";
+import { Tag } from "src/modules/tags/domain/entities/tags.entity";
+import { User } from "src/modules/users/domain/entities/users.entity";
+import { Venue } from "src/modules/venues/domain/entities/venues.entity";
+import { VenueScheduleItem } from "src/modules/venues/domain/entities/venueScheduleItem.entity";
+import { CommunicationConfirm } from "src/modules/verification-codes/domain/entities/communication-confirm.entity";
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+        useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+
+        host: config.get<string>('DB_HOST'),
+        port: Number(config.get<number>('DB_PORT')),
+
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASS'),
+        database: config.get<string>('DB_NAME'),
+
+        synchronize: config.get<string>('DB_SYNC') !== 'false',
+        logging: config.get<string>('DB_DEBUG') !== 'false',
+
+        autoLoadEntities: true,
+      })
+    }),
+    TypeOrmModule.forFeature([
+			Comment,
+			Communication,
+			Event,
+			Resource,
+			Tag,
+			User,
+			Venue,
+			VenueScheduleItem,
+			CommunicationConfirm,
+		]),
+  ]
+})
+export class TOrmModule {}
